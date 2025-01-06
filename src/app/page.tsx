5b80bd656ba35fb64e272d3d004bd4b36cda1486
LuTanDat <ludathoc@gmail.com>
2025-01-06 17:37:01 +0700

    Sử Dụng CSS với Next.JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link'
+import app from '@/styles/app.module.css';
+import app2 from '@/styles/app2.module.css';
 
 export default function Home() {
   return (
     <div>
       <ul>
-        <li>
-          <Link href="/facebook">Facebook</Link>
+        <li className={app['red']}>
+          <Link href="/facebook">
+            <span className={app2['red']}>Facebook</span>
+          </Link>
           </li>
         <li><a href="/youtube">Youtube</a></li>
         <li><a href="/tiktok">Tiktok</a></li>
