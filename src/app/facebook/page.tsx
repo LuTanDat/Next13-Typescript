3602e6555ad1dd12df747904b0822718f755d34b
LuTanDat <ludathoc@gmail.com>
2025-01-06 10:59:53 +0700

    test-router

---
+++ src/app/facebook/page.tsx
@@ -0,0 +1,21 @@
+'use client' // su dung event cua client
+import { useRouter } from 'next/navigation'
+
+const Facebook = () => {
+  const router = useRouter()
+
+  const handleBtn = () => {
+    router.push('/')
+  }
+
+  return (
+    <div>
+      Facebook
+      <div>
+        <button onClick={() => handleBtn()}>Back Home</button>
+      </div>
+    </div>
+  )
+}
+
+export default Facebook;
---
+++ src/app/youtube/page.tsx
@@ -0,0 +1,10 @@
+
+const Youtube = () => {
+  return (
+    <div>
+      Youtube
+    </div>
+  )
+}
+
+export default Youtube;
