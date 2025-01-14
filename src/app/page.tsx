931131bf10ede538268dc78eacb23e54cd934d77
LuTanDat <ludathoc@gmail.com>
2025-01-14 20:55:17 +0700

    format structure blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,9 @@
 import Link from 'next/link'
 import app from '@/styles/app.module.css';
 import app2 from '@/styles/app2.module.css';
-import TablePage from '@/components/app.table';
-import { useEffect } from 'react';
-import useSWR from 'swr';
 
 export default function Home() {
 
-  const fetcher = (url: string) => fetch(url).then(res => res.json())
-
-  const { data, error, isLoading } = useSWR(
-    'http://localhost:8000/blogs',
-    fetcher,
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false
-    }
-  )
-
-  // console.log('>>> check data: ' ,data); // [{}, {}, {}, {}, {}, {}, {},...]
-
-  if (!data) return <div>Loading...</div>
   return (
     <div>
       <ul>
@@ -39,10 +21,6 @@ export default function Home() {
           <Link href="/tiktok">Tiktok</Link>
         </li>
       </ul>
-
-      <TablePage
-        blogs={data?.sort((a: any, b: any) => b.id - a.id)}
-      />
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,11 +17,11 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link href="/facebook" className='nav-link'>Facebook</Link>
-            <Link href="/youtube" className='nav-link'>Youtube</Link>
-            <Link href="/tiktok" className='nav-link'>Tiktok</Link>
+            <Link href="/blogs" className='nav-link'>Blogs</Link>
 
-          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            {/* <Link href="/youtube" className='nav-link'>Youtube</Link>
+            <Link href="/tiktok" className='nav-link'>Tiktok</Link> */}
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
